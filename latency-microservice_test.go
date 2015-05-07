ce2a12c5d5db6aad03c88b2cabe01176139b6803
Haukur Kristinsson <haukur@hauxi.is>
2015-05-07 12:46:37 -0400

    Added pinger testers, to check if you have raw socket access from the kernel

--- latency-microservice_test.go
+++ latency-microservice_test.go
@@ -2,10 +2,69 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"testing"
+	"time"
 
 	"github.com/haukurk/latency-microservice-go/client"
+	"github.com/tatsushid/go-fastping"
 )
+
+func TestPinger(t *testing.T) {
+
+	p := fastping.NewPinger()
+	p.Network("ip")
+
+	if err := p.AddIP("127.0.0.1"); err != nil {
+		t.Fatalf("AddIP failed: %v", err)
+	}
+
+	if err := p.AddIP("1.1.1.1"); err != nil {
+		t.Fatalf("AddIP failed: %v", err)
+	}
+
+	if err := p.AddIP("::1"); err != nil {
+		t.Fatalf("AddIP failed: %v", err)
+	}
+
+	found1, found100, foundv6 := false, false, false
+	called, idle := false, false
+	p.OnRecv = func(ip *net.IPAddr, d time.Duration) {
+		called = true
+		if ip.String() == "127.0.0.1" {
+			found1 = true
+		} else if ip.String() == "127.0.0.100" {
+			found100 = true
+		} else if ip.String() == "::1" {
+			foundv6 = true
+		}
+	}
+
+	p.OnIdle = func() {
+		idle = true
+	}
+
+	err := p.Run()
+	if err != nil {
+		t.Fatalf("Pinger returns error: %v", err)
+	}
+	if !called {
+		t.Fatalf("Pinger didn't get any responses")
+	}
+	if !idle {
+		t.Fatalf("Pinger didn't call OnIdle function")
+	}
+	if !found1 {
+		t.Fatalf("Pinger `127.0.0.1` didn't respond")
+	}
+	if found100 {
+		t.Fatalf("Pinger `127.0.0.100` responded")
+	}
+	if !foundv6 {
+		t.Fatalf("Pinger `::1` didn't responded")
+	}
+
+}
 
 func TestPingIPEndpointSuccess(t *testing.T) {
 
