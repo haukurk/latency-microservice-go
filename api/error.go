6739908def674dd02996b9ed61e8a499f20ac8a9
Haukur Kristinsson <haukur@hauxi.is>
2015-05-07 07:37:53 -0400

    Updated error spec

--- api/error.go
+++ api/error.go
@@ -1,9 +1,10 @@
 package api
 
 type Error struct {
-	Error string `json:"error"`
+	Error  string `json:"error"`
+	Status string `json:"status"`
 }
 
 func NewError(msg string) *Error {
-	return &Error{Error: msg}
+	return &Error{Error: msg, Status: "fail"}
 }
