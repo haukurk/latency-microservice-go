e689fedefc213d7040763cd28bec9c870c29aa58
Haukur Kristinsson <haukur@hauxi.is>
2015-05-10 07:51:48 -0400

    adding stats endpoint

--- service/latency.go
+++ service/latency.go
@@ -2,9 +2,14 @@ package service
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/thoas/stats"
 )
+
+var Stats = stats.New()
 
 type Config struct {
 	Port int
@@ -14,8 +19,23 @@ type LatencyService struct{}
 
 func (s *LatencyService) Run(cfg Config) error {
 	latencyResource := &LatencyResource{}
+
 	router := gin.Default()
+
+	router.Use(func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			beginning := time.Now()
+			c.Next()
+			Stats.End(beginning, c.Writer)
+		}
+	}())
+
+	router.GET("/stats", func(c *gin.Context) {
+		c.JSON(http.StatusOK, Stats.Data())
+	})
+
 	router.GET("/latency/:host", latencyResource.LatencyHost)
+
 	router.Run(fmt.Sprint(":", cfg.Port))
 
 	return nil
