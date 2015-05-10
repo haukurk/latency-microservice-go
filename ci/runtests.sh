d44d8fa1d8167f5951483922c57d83f489fcf9dc
Haukur Kristinsson <haukur@hauxi.is>
2015-05-10 12:58:35 -0400

    fixing CI tests because of server-cmd change

--- ci/runtests.sh
+++ ci/runtests.sh
@@ -3,6 +3,6 @@ pwd
 echo $1
 export GOROOT=$1
 export GOPATH=/home/travis/gopath/
-./cmd/server/server --config config.json server & \
+./cmd/latency-server/latency-server --config config.json server & \
 pid=$$!; \
 $1/bin/go test 
